87b2e7f80bf3a555498e4d3d42685d7300c7fb79
paulpellom@gmail.com <paulpellom@gmail.com>
2018-04-05 22:19:46 -0400

    init

---
+++ portfolio_on_rails/db/message_data.rb
@@ -0,0 +1,38 @@
+def get_message_data
+
+    message_data = {
+
+    'hello' => {
+        title: 'Hey There',
+        text: 'Hey bud just wanted to drop a line, to see how your doing. Portfolio is looking sweet af! TTYLbuddy!',
+        date: 03042018
+    },
+    
+    'hello' => {
+        title: 'Whats up',
+        text: 'Here is some example text, hopefully no one will have to read this. ',
+        date: 03042018
+    },
+
+    'hello' => {
+        title: 'Peemps in SPPPAAACE',
+        text: 'Dude, I just saw this super awesome movie. I will tell you about it this saturday at my place!',
+        date: 03042018
+    },
+
+    'hello' => {
+        title: 'What a day what a day',
+        text: 'just wanted to express my most deep condolences. Its been pretty crazy lately, maybe soon you can get a break ',
+        date: 03042018
+    },
+    
+    'hello' => {
+        title: 'Moonlight Sonata',
+        text: 'dum doo doo dum doo doo dun dooddooo dooooo'
+        date: 03042018
+    }
+
+    }
+    message_data
+end
+puts "Sucessfully seeded message data"
